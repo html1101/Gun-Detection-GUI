4583977f643c02f57fbc80dff1dedcb3d5f91617
Jasmine <hollis.e.jasmine@gmail.com>
2022-01-19 07:42:39 -0600

    starting users page and cameras page edits

---
+++ Frontend/users.js
@@ -0,0 +1,8 @@
+// const user = {
+//    firstName: "Sarah",
+//    lastName: "Cross",
+//    phone: "210-765-4321",
+//    email: "sc@scifair.com"
+//};
+
+// document.getElementById("body").innerHTML = user["firstName"] + ", " + user["lastName"] + ", " + user["phone"] + ", " + user["email"];
