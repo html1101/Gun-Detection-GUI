663f4fea38a3944a6d848c0ec55f4986f0d80400
Jasmine <hollis.e.jasmine@gmail.com>
2022-01-20 21:30:26 -0600

    more work on users page

--- Frontend/users.js
+++ Frontend/users.js
@@ -1,8 +1,15 @@
-// const user = {
-//    firstName: "Sarah",
-//    lastName: "Cross",
-//    phone: "210-765-4321",
-//    email: "sc@scifair.com"
-//};
+const user = {
+   firstName: "Sarah",
+   lastName: "Cross",
+   phone: "210-765-4321",
+   email: "sc@scifair.com"
+};
 
-// document.getElementById("body").innerHTML = user["firstName"] + ", " + user["lastName"] + ", " + user["phone"] + ", " + user["email"];
+
+document.getElementById("user_info").innerHTML = `
+${user["firstName"]} ${user["lastName"]}
+<br>
+${user["phone"]}
+<br>
+${user["email"]}
+`;
